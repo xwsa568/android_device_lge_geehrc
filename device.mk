5ab6db7019e3d906965f9aa2d542b83d4d93cccf
Kevin <98kevchen@gmail.com>
2014-11-16 16:34:56 +0100

    geehrc: device: rm nfc.hce (doesnt go well with nxp nfc chipset)

--- device.mk
+++ device.mk
@@ -124,7 +124,6 @@ PRODUCT_COPY_FILES += \
     $(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     device/lge/geehrc/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf
 
 PRODUCT_PROPERTY_OVERRIDES += \
