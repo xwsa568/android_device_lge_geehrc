b6eb9de21e01357d4557df152a47daadee105178
Kevin <98kevchen@gmail.com>
2014-11-22 16:50:40 +0100

    geehrc: prepare for custom hotplug drivers
    * remove power device specific bits (power.geehrc)
    * disable mpdecision -> replaceage by auto_hotplug in kernel
    * remove deprecated mpdecision touchboost flasg in BoardConfig.mk

--- device.mk
+++ device.mk
@@ -241,9 +241,6 @@ PRODUCT_PACKAGES += \
 	wpa_supplicant_overlay.conf \
 	p2p_supplicant_overlay.conf
 
-PRODUCT_PACKAGES += \
-	power.geehrc
-
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	rild.libpath=/system/lib/libril-qc-qmi-1.so
 
