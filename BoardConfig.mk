944eac7c25a42f5dfca32b8f93efa89a3c0f53d7
Kevindeving <98kevchen@gmail.com>
2014-11-27 12:49:10 -0500

    geehrc: add all common optimus g asserts

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -183,3 +183,6 @@ MR_DPI := hdpi
 MR_DPI_FONT := 216
 MR_FSTAB := device/lge/geehrc/recovery/twrp.fstab
 MR_KEXEC_MEM_MIN := 0x84000000
+
+#Asserts
+TARGET_OTA_ASSERT_DEVICE := gee,geeb,geehrc,e975,geehrc_4g_spr,geespr,ls970,geehrc4g,geehrc4g_spr_us,geebus,e970,e973,e971,geeb_att_us
