f4d681f1633b850e6cd5b98ae79c5534b83f60ea
jollaman999 <admin@jollaman999.com>
2014-12-23 22:24:36 +0900

    device.mk: Fix handset mic option

--- device.mk
+++ device.mk
@@ -139,7 +139,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	persist.audio.handset.mic.type=digital \
 	persist.audio.dualmic.config=endfire \
 	persist.audio.fluence.voicecall=true \
-	persist.audio.handset.mic=dmic \
+	persist.audio.handset.mic=digital \
 	persist.audio.fluence.mode=endfire \
 	persist.audio.lowlatency.rec=false
 
