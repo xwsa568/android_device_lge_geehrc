169395c7e276d911e64dac12aa7d16b340f4ea2b
jollaman999 <admin@jollaman999.com>
2014-12-27 18:48:15 +0900

    wifi: Android.mk: Remove local include path

--- wifi/Android.mk
+++ wifi/Android.mk
@@ -25,8 +25,6 @@
 
 ifneq ($(filter geehrc,$(TARGET_DEVICE)),)
 
-LOCAL_PATH := $(call my-dir)
-
 include $(CLEAR_VARS)
 LOCAL_MODULE       := wpa_supplicant_overlay.conf
 LOCAL_MODULE_TAGS  := optional
@@ -50,8 +48,6 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := $(LOCAL_MODULE)
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/hostapd
 include $(BUILD_PREBUILT)
-
-include $(call first-makefiles-under,$(LOCAL_PATH))
 
 endif
 
